b767675a7a1e989f535186c8cb01ddd1251f2b22
Vasya-sesh <gf2905678@gmail.com>
2021-06-22 16:28:39 +0300

    Testing git

---
+++ src/test/java/org/example/FinalTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.Test;
+
+public class FinalTest {
+    @Test
+    public void messageEncodingAndDecodingTest() throws Exception
+    {
+        System.out.println("Trying out git");
+    }
+}
